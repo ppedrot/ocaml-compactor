4b665bf62d771055cc5bf2ac529a8ee909e231dd
Pierre-Marie Pédrot <pierre-marie.pedrot@inria.fr>
2012-10-21 04:17:01 +0200

    Small fix in .h

--- copt/buffer.h
+++ copt/buffer.h
@@ -6,6 +6,6 @@ buffer* buffer_init(int size);
 void buffer_free(buffer* b);
 int buffer_pop(buffer* b);
 void buffer_push(buffer* b, int i);
-int buffer_length();
+int buffer_length(buffer* b);
 int* buffer_contents(buffer* b);
 void buffer_clear(buffer* b);
